c8c3754718b46a761242cf5129854d52acccc2f6
Lucas Candido de Souza Silva <lcdss@live.com>
2020-02-29 03:34:30 -0300

    fix: add missing validations and other configs

--- api/resources/lang/en/validation.php
+++ api/resources/lang/en/validation.php
@@ -4,6 +4,12 @@ return [
 
     'cpf' => 'The :attribute field is not valid.',
     'email' => 'The :attribute must be a valid email address.',
+    'image' => 'The :attribute must be an image.',
+    'max' => [
+        'file' => 'The :attribute may not be greater than :max kilobytes.',
+        'string' => 'The :attribute may not be greater than :max characters.',
+    ],
+    'mimes' => 'The :attribute must be a file of type: :values.',
     'required' => 'The :attribute field is required.',
     'unique' => 'The :attribute has already been taken.',
 
